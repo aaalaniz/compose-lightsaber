9ca59cc0f11f993582de2298790f1f9b29b5240d
Aaron Alaniz <aaron@aalaniz.com>
2023-09-19 18:29:03 -0500

    Inject navigator from root composable (#6)

--- shared/src/iosMain/kotlin/xyz/alaniz/aaron/lightsaber/di/IosApplicationComponent.kt
+++ shared/src/iosMain/kotlin/xyz/alaniz/aaron/lightsaber/di/IosApplicationComponent.kt
@@ -1,15 +1,15 @@
 package xyz.alaniz.aaron.lightsaber.di
 
+import com.slack.circuit.runtime.Navigator
 import kotlinx.coroutines.CoroutineScope
 import me.tatarka.inject.annotations.Component
 import me.tatarka.inject.annotations.Provides
-import xyz.alaniz.aaron.lightsaber.ui.common.InitialScreen
-import xyz.alaniz.aaron.lightsaber.ui.lightsaber.IosLightsaberScreen
 import xyz.alaniz.aaron.lightsaber.ui.settings.IosSettingsScreen
 import xyz.alaniz.aaron.lightsaber.ui.settings.SettingsScreen
 
 @Component
 abstract class IosApplicationComponent(
+    @get:Provides protected val navigator: Navigator,
     @get:Provides protected val appScope: CoroutineScope,
     dataStorePath: String,
     @Component protected val iosSoundComponent: IosSoundComponent = IosSoundComponent::class.create(),
@@ -19,10 +19,6 @@ abstract class IosApplicationComponent(
         dataStorePath
     )
 ) : ApplicationComponent() {
-    @Provides
-    protected fun provideInitialScreen(lightsaberScreen: IosLightsaberScreen): InitialScreen =
-        InitialScreen(lightsaberScreen)
-
     protected val IosSettingsScreen.bind: SettingsScreen
         @Provides get() = this
 }
