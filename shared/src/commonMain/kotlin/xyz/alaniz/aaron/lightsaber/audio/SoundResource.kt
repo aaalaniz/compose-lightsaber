906dcb2202baa9e0a211d098d3a5d0c4c9857d89
Aaron Alaniz <aaron@aalaniz.com>
2024-03-01 21:05:16 -0600

    Fix release builds

--- shared/src/commonMain/kotlin/xyz/alaniz/aaron/lightsaber/audio/SoundResource.kt
+++ shared/src/commonMain/kotlin/xyz/alaniz/aaron/lightsaber/audio/SoundResource.kt
@@ -4,5 +4,5 @@ package xyz.alaniz.aaron.lightsaber.audio
 class SoundResource(
     val name: String,
     val fileType: String,
-    internal val directory: String = "files",
+    internal val directory: String = "files/raw",
 )
