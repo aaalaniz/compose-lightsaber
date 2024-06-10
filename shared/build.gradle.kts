f21516a541efc1b503bcb14fb88083695c656e66
Aaron Alaniz <aaron@aalaniz.com>
2024-06-09 22:08:27 -0500

    Upgrade all deps to Kotlin 2.0.0 compatible versions

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.library)
     alias(libs.plugins.compose)
+    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.ksp)
     alias(libs.plugins.kotlin.plugin.parcelize)
     alias(libs.plugins.kotlin.native.cocoapods)
