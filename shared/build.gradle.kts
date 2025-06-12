06e5f920a5e05e4050df4568c85a96f3a4198bd4
Aaron Alaniz <aaron@aalaniz.com>
2025-06-12 15:10:07 -0500

    Update deps

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -54,6 +54,7 @@ kotlin {
                 implementation(compose.runtime)
                 implementation(compose.foundation)
                 implementation(compose.material)
+                implementation(compose.materialIconsExtended)
                 @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
                 implementation(compose.components.resources)
                 implementation(libs.kotlin.inject.runtime.kmp)
